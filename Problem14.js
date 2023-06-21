b00c4d609e30f18d14d18eec63f724147c0e906c
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-06-21 17:10:05 +0530

    initial commit

--- Problem14.js
+++ Problem14.js
@@ -1,1 +1,22 @@
-//Candy Distribution
+//Candy
+
+var candy = function (ratings) {
+  let candy = new Array(ratings.length).fill(1);
+
+  // left to right
+  for (let i = 1; i < ratings.length; i++) {
+    if (ratings[i] > ratings[i - 1] && candy[i] <= candy[i - 1]) {
+      candy[i] = candy[i - 1] + 1;
+    }
+  }
+
+  // right to left
+  for (let i = ratings.length - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
+      candy[i] = candy[i + 1] + 1;
+    }
+  }
+
+  let sum = candy.reduce((cur, acc) => (acc = cur + acc));
+  return sum;
+};
