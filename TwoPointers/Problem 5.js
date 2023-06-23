68914e2714d4c1fb2d345310217c4317d365a85b
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-06-23 18:41:26 +0530

    Two Pointers

---
+++ TwoPointers/Problem 5.js
@@ -0,0 +1,28 @@
+//3Sum
+var threeSum = function (nums) {
+  let res = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+    while (l < r) {
+      let threeSum = nums[i] + nums[l] + nums[r];
+      if (threeSum > 0) {
+        r -= 1;
+      } else if (threeSum < 0) {
+        l += 1;
+      } else {
+        res.push([nums[i], nums[l], nums[r]]);
+        l += 1;
+        while (nums[l] === nums[l - 1] && l < r) {
+          l += 1;
+        }
+      }
+    }
+  }
+  return res;
+};
